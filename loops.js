a5ddd1e011700ef2d35e5c1eb041233ebcf60301
Archi Misra <misraarchi117@gmail.com>
2026-03-12 00:29:31 +0530

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,16 @@
+for(let i=1;i<=6;i++){
+   console.log("Archi Misra");
+}
+
+let i = 1;
+while(i<=10){
+    console.log("archi misra");
+    i++; 
+}
+let str = "ArchiMisra";
+let size = 0;
+for(let i of str){
+    console.log("i=",i);
+    size++;
+}
+console.log("string size = ",size);
